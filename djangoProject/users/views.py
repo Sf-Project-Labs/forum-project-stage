1a4ec7ef07e38bf6693b16e2938cca94396ce1ec
Simeon <simeonxsimeonov@gmail.com>
2024-12-01 23:10:21 +0200

    added User model and modified URL paths

--- djangoProject/users/views.py
+++ djangoProject/users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 from rest_framework import status, generics
-from rest_framework.response import Response
+
 from .serializers import UserRegistrationSerializer
 from .models import User
 
