416f843eb16f186cb8127e1b443f726b945de25e
Vladyslav <kramarenko.creator@gmail.com>
2024-12-07 16:04:00 +0200

    feat: Implement user sign-out functionality
    - Invalidate JWT tokens upon user logout by introducing a token blacklist strategy.
    - Create a Sign Out API endpoint to add tokens to the blacklist during logout.
    - Ensure client-side token deletion upon user logout for improved security.

--- djangoProject/users/urls.py
+++ djangoProject/users/urls.py
@@ -1,9 +1,11 @@
 
 from django.urls import path, include
-from .views import LoginView, home, UserRegistrationView
+from .views import LoginView, LogoutView, home, UserRegistrationView
 
 urlpatterns = [
     path('', home, name='home'),
     path('sign-up/', UserRegistrationView.as_view(), name='sign-up'),
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
+
