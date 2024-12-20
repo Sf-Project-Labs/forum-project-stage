825aa6671b4dcfe92e952a6d6689c345c5d6cf84
Vladyslav <kramarenko.creator@gmail.com>
2024-12-21 01:08:50 +0200

    Updated wait-for-it.sh, removed health_check as it is already included, and added necessary packages to requirements.txt

--- djangoProject/users/urls.py
+++ djangoProject/users/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import LoginView, LogoutView, home, UserRegistrationView, health_check
+from .views import LoginView, LogoutView, home, UserRegistrationView
 
 urlpatterns = [
     path('', home, name='home'),
     path('register/', UserRegistrationView.as_view(), name='register'),
-    path('health/', health_check, name='health'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
