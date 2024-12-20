1f2d227fe0dd86cff86413c3cbd419843410c6fd
Niki5225 <122698681+Niki5225@users.noreply.github.com>
2024-12-20 20:05:07 +0200

    define user model and remove unnecessary field in user model

--- djangoProject/profiles/models.py
+++ djangoProject/profiles/models.py
@@ -1,3 +1,54 @@
+from django.core.validators import MinLengthValidator, RegexValidator
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class BaseProfile(models.Model):
+    MIN_LENGTH_DEFAULT = 3
+    MAX_LENGTH_DEFAULT = 50
+    PHONE_NUMBER_MAX_LEN = 15
+
+    company_name = models.CharField(
+        unique=True,
+        max_length=MAX_LENGTH_DEFAULT,
+        validators=[MinLengthValidator(MIN_LENGTH_DEFAULT)],
+    )
+
+    legal_name = models.CharField(
+        unique=True,
+        max_length=MAX_LENGTH_DEFAULT,
+        validators=[MinLengthValidator(MIN_LENGTH_DEFAULT)],
+    )
+
+    email = models.EmailField(
+        unique=True,
+        null=True,
+        blank=True,
+    )
+
+    phone_number = PhoneNumberField(
+        blank=True,
+        null=True,
+    )
+
+    region = models.CharField(
+        max_length=MAX_LENGTH_DEFAULT,
+        validators=[MinLengthValidator(MIN_LENGTH_DEFAULT)],
+        null=True,
+        blank=True,
+    )
+
+    industry_type = models.CharField(
+        max_length=MAX_LENGTH_DEFAULT,
+        validators=[MinLengthValidator(MIN_LENGTH_DEFAULT)],
+        null=True,
+        blank=True,
+    )
+
+
+class StartUpProfile(BaseProfile):
+    project_information = models.TextField()
+
+
+class InvestorProfile(BaseProfile):
+    pass
